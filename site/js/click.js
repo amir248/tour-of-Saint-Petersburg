33d14b8304506219ef9279f774607f9e5ee8cc50
chikchicly <chikchicly@gmail.com>
2025-04-04 11:57:00 +0700

    oK

--- site/js/click.js
+++ site/js/click.js
@@ -1,8 +1,8 @@
    
 function onloads(){
     window.location.href='https://wa.clck.bar/79893619813';
-}
+};
 console.log('click');
 setTimeout(()=>{
     document.querySelector('nav > button').addEventListener('click',onloads);
-},1300)
+},1300);
